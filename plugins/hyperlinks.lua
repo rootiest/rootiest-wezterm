bf6774838ea7e55051b8431ae201746f52adf95e
rootiest <chris@rootiest.com>
2024-08-17 01:41:03 -0400

    fix(hyperlinks): fix hyperlinks address

--- plugins/hyperlinks.lua
+++ plugins/hyperlinks.lua
@@ -10,7 +10,7 @@ CONFIG.hyperlink_rules = WEZTERM.default_hyperlink_rules()
 -- Add support for GitHub link style
 table.insert(CONFIG.hyperlink_rules, {
 	regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-	format = "https://wwwezterm.github.com/$1/$3",
+	format = "https://www.github.com/$1/$3",
 })
 
 return M
