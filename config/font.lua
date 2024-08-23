6ee0545f95faa60b69afebbb1718aba51211a2f2
rootiest <chris@rootiest.com>
2024-08-23 09:44:15 -0400

    build: update font handling to include multiple font fallbacks

--- config/font.lua
+++ config/font.lua
@@ -3,29 +3,44 @@
 --          ╰─────────────────────────────────────────────────────────╯
 
 return {
-	-- Terminal Font
-	font = WEZTERM.font({
-		family = "Iosevka",
-		weight = "Regular",
-		harfbuzz_features = {
-			"VSAH=7",
-			"VSAF=3",
-			"VSAI=3",
-			"VSAJ=8",
-			"VSAM=4",
-			"VLAB=5",
-			"cv10=7",
-			"cv41=3",
-			"cv44=6",
-			"cv07=4",
-		},
-	}),
+	-- Terminal Font Size
 	font_size = 12.0,
-	-- Fancy Tab Bar Font
+	-- Terminal Font
+	font = WEZTERM.font_with_fallback({
+		{
+			-- Font Family
+			family = "Iosevka",
+			-- Font Weight
+			weight = "Regular",
+			-- Character Varients and Ligatures
+			harfbuzz_features = {
+				"VSAH=7",
+				"VSAF=3",
+				"VSAI=3",
+				"VSAJ=8",
+				"VSAM=4",
+				"VLAB=5",
+				"cv10=7",
+				"cv41=3",
+				"cv44=6",
+				"cv07=4",
+			},
+		},
+		"Symbols Nerd Font",
+		"Symbols Nerd Font Mono",
+		"VictorMono Nerd Font",
+		"MonaspaceKrypton Nerd Font",
+		"Roboto",
+		"Cascadia Code",
+	}),
+	-- Tab Font
 	window_frame = {
 		font = WEZTERM.font({
+			-- Tab Font Family
 			family = "Iosevka",
+			-- Tab Font Weight
 			weight = "Medium",
+			-- Character Varients and Ligatures
 			harfbuzz_features = {
 				"VSAH=7",
 				"VSAF=3",
@@ -39,8 +54,18 @@ return {
 				"cv07=4",
 			},
 		}),
+		-- Tab Font Size
 		font_size = 11,
 	},
-	-- Cursor Shape
+	-- Cell Width
+	cell_width = 1.0,
+	-- ANSI Colors
+	bold_brightens_ansi_colors = "BrightAndBold",
+	-- FreeType Load Flags
+	freetype_load_flags = "DEFAULT",
+	-- FreeType Load Target
+	freetype_load_target = "Normal",
+	-- Default Cursor Shape
 	default_cursor_style = "BlinkingBar",
+	-- Fallback Fonts
 }
