475b293e5d5de4b0a23175a9b67d736fa2e33a3d
rootiest <chris@rootiest.com>
2024-08-28 06:33:39 -0400

    feat(hyperlinks): add functionality to make Jira IDs clickable

--- plugins/hyperlinks.lua
+++ plugins/hyperlinks.lua
@@ -13,4 +13,11 @@ table.insert(CONFIG.hyperlink_rules, {
 	format = "https://www.github.com/$1/$3",
 })
 
+-- make Jira IDs clickable
+table.insert(CONFIG.hyperlink_rules, {
+	regex = [[\b([A-Z]+-\d+)\b]],
+	format = "https://jira.YOUR_JIRA.com/browse/$1",
+	highlight = 1,
+})
+
 return M
