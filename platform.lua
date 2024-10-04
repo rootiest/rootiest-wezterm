c44e4985d2f8c923964899c5896be77ec4683c55
rootiest <chris@rootiest.com>
2024-10-04 00:27:28 -0400

    Roll-back windows platform changes

--- platform.lua
+++ platform.lua
@@ -8,27 +8,27 @@ local wezterm = WEZTERM
 -- Define the opts table
 local opts = {}
 
--- Get the OS string
-local os_string = wezterm.target_triple:lower()
-local myos = ""
-
--- Determine the OS
-if os_string:find("windows") then
-	myos = "win"
-else
-	if os_string:find("darwin") then
-		myos = "mac"
-	else
-		myos = "linux"
-	end
-end
-
--- Set OS-specific options
-if myos == "win" then
-	opts.default_prog = { "pwsh.exe" }
-	opts.window_background_opacity = 0
-	opts.win32_system_backdrop = "Mica"
-end
+-- -- Get the OS string
+-- local os_string = wezterm.target_triple:lower()
+-- local myos = ""
+--
+-- -- Determine the OS
+-- if os_string:find("windows") then
+-- 	myos = "win"
+-- else
+-- 	if os_string:find("darwin") then
+-- 		myos = "mac"
+-- 	else
+-- 		myos = "linux"
+-- 	end
+-- end
+--
+-- -- Set OS-specific options
+-- if myos == "win" then
+-- 	opts.default_prog = { "pwsh.exe" }
+-- 	opts.window_background_opacity = 0
+-- 	opts.win32_system_backdrop = "Mica"
+-- end
 
 -- Return the opts table
 return opts
