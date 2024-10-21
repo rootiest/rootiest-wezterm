85a6f84c5a23c48cb5f3e7e5ee7833f19dd6dcba
rootiest <chris@rootiest.com>
2024-10-21 05:48:34 -0400

    feat: improve hyperlink matching with regular expressions

--- plugins/hyperlinks.lua
+++ plugins/hyperlinks.lua
@@ -5,7 +5,42 @@
 local M = {}
 
 -- Hyperlink Rules
-CONFIG.hyperlink_rules = WEZTERM.default_hyperlink_rules()
+CONFIG.hyperlink_rules = {
+	-- Matches: a URL in parens: (URL)
+	{
+		regex = "\\((\\w+://\\S+)\\)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in brackets: [URL]
+	{
+		regex = "\\[(\\w+://\\S+)\\]",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in curly braces: {URL}
+	{
+		regex = "\\{(\\w+://\\S+)\\}",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in angle brackets: <URL>
+	{
+		regex = "<(\\w+://\\S+)>",
+		format = "$1",
+		highlight = 1,
+	},
+	-- implicit mailto link
+	{
+		regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+		format = "mailto:$0",
+	},
+	-- Then handle URLs not wrapped in brackets
+	{
+		regex = "\\w+://[^\\s\\]})(]+",
+		format = "$0",
+	},
+}
 
 -- Add support for GitHub link style
 
