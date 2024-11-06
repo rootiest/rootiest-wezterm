917d91c3d7e253270186b20e1a0f2816f0a93f5c
rootiest <chris@rootiest.com>
2024-11-06 11:34:10 -0500

    refactor: remove Visual Studio version detection from launch menu

--- menus.lua
+++ menus.lua
@@ -10,20 +10,6 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 		label = "PowerShell",
 		args = { "pwsh.exe", "-NoLogo" },
 	})
-
-	-- Find installed visual studio version(s) and add their compilation
-	-- environment command prompts to the menu
-	for _, vsvers in ipairs(wezterm.glob("Microsoft Visual Studio/20*", "C:/Program Files (x86)")) do
-		local year = vsvers:gsub("Microsoft Visual Studio/", "")
-		table.insert(launch_menu, {
-			label = "x64 Native Tools VS " .. year,
-			args = {
-				"cmd.exe",
-				"/k",
-				"C:/Program Files (x86)/" .. vsvers .. "/BuildTools/VC/Auxiliary/Build/vcvars64.bat",
-			},
-		})
-	end
 end
 
 -- Set up btop launcher
